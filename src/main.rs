085ab070dd69a9fd55efa11f44f3592ecae23e26
Johan Lorenzo <jlorenzo@mozilla.com>
2016-01-29 15:28:34 +0100

    Add first test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+fn add_world(foo: &str) -> String {
+    let mut bar = foo.to_string();
+    bar.push_str(" world");
+    return bar;
+}
+
+#[test]
+fn test_should_add_world() {
+    assert_eq!("Hello world", add_world("Hello"));
 }
